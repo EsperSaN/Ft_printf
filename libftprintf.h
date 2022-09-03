224a242f16ca6cfb677ea8d5c9f7e84aecf9e095
Polawat Ruenruay <pruenrua@z3t2c4.42bangkok.com>
2022-09-03 16:14:53 +0700

    add struct

---
+++ libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pruenrua <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/03 15:55:31 by pruenrua          #+#    #+#             */
+/*   Updated: 2022/09/03 16:10:44 by pruenrua         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+int		ft_printf(const char *, ...);
+
+#endif
