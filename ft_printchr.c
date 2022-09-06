dd919f26dd1f8cc55670b8b94af3ecdd05386f6e
EsperSaN <polawat.ruenruay@hotmail.com>
2022-09-07 01:21:31 +0700

    update NULL condition

--- ft_printchr.c
+++ ft_printchr.c
@@ -3,11 +3,49 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printchr.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: eszu <eszu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 16:11:55 by pruenrua          #+#    #+#             */
-/*   Updated: 2022/09/06 20:22:42 by marvin           ###   ########.fr       */
+/*   Updated: 2022/09/07 01:18:21 by eszu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+
+int ft_strlen_f(char *str)
+{
+    int  len;
+
+    len = 0; 
+    while(*str)
+    {
+        str++;
+        len++;
+    }
+    return (len);
+}
+
+int ft_putchar_f(int c)
+{
+    write(1, &c, 1);
+    return (1);
+}
+
+int ft_putstring_f(char *str)
+{
+    int op_count;
+
+    op_count = 0;
+
+    if (str == NULL)
+    {
+        write(1, "(NULL)", 6);
+        op_count += 6;   
+    }
+    while (*str)
+    {
+        op_count += ft_putchar_f(str);
+        str++;
+    }
+    return (op_count);
+}
--- ft_printnbr.c
+++ ft_printnbr.c
@@ -6,43 +6,11 @@
 /*   By: eszu <eszu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 16:11:20 by pruenrua          #+#    #+#             */
-/*   Updated: 2022/09/07 00:40:06 by eszu             ###   ########.fr       */
+/*   Updated: 2022/09/07 01:21:13 by eszu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
-
-int ft_strlen_f(char *str)
-{
-    int  len;
-
-    len = 0;
-    while(*str)
-    {
-        str++;
-        len++;
-    }
-    return (len);
-}
-
-int ft_putchar_f(int c)
-{
-    write(1, &c, 1);
-    return (1);
-}
-
-int ft_putstring_f(char *str)
-{
-    int op_count;
-
-    op_count = 0;
-    while (*str)
-    {
-        op_count += ft_putchar_f(str);
-        str++;
-    }
-    return (op_count);
-}
 
 int ft_print_nbr_base(long value, char *base)
 {
@@ -52,6 +20,8 @@ int ft_print_nbr_base(long value, char *base)
 
     base_count = ft_strlen_f(base);
     op_count = 0;
+    if (value == 0)
+        op_count += ft_putchar_f('0');
     if (value < 0);
     {
         value = (value * -1);
@@ -68,11 +38,13 @@ int ft_print_nbr_base(long value, char *base)
 int ft_print_nbr_base_u(unsigned long value, char *base)
 {
     unsigned long    result;
-    int     op_count;
-    int     base_count;
+    int              op_count;
+    int              base_count;
 
     base_count = ft_strlen_f(base);
     op_count = 0;
+    if (value == 0)
+        op_count += ft_putchar_f('0');
     while (value)
     {
         op_count += ft_putchar_f(base[(value % base_count)]);
